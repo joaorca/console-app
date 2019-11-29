b0120d61619b4a04ec03f5f93b851f74cc84ad5d
João R C Almeida <joaorca@gmail.com>
2019-11-29 17:51:58 -0300

    Implementando Console

---
+++ src/App/Command/DefaultCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace App\Command;
+
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableSeparator;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class DefaultCommand extends Command
+{
+    // the name of the command (the part after "bin/console")
+    protected static $defaultName = 'app:default';
+
+    protected function configure()
+    {
+        $this
+            // the short description shown while running "php bin/console list"
+            ->setDescription('Creates a new user.')
+
+            // the full command description shown when running the command with
+            // the "--help" option
+            ->setHelp('This command allows you to create a user...');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        //https://symfony.com/doc/current/console/style.html
+        $io = new SymfonyStyle($input, $output);
+
+        $table = new Table($output);
+        $table
+            ->setStyle('box-double')
+            ->setHeaders(['ISBN', 'Title', 'Author'])
+            ->setRows([
+                ['99921-58-10-7', 'Divine Comedy', 'Dante Alighieri'],
+                ['9971-5-0210-0', 'A Tale of Two Cities', 'Charles Dickens'],
+                ['960-425-059-0', 'The Lord of the Rings', 'J. R. R. Tolkien'],
+                ['80-902734-1-6', 'And Then There Were None', 'Agatha Christie'],
+            ]);
+        $table->render();
+
+        return 0;
+    }
+}
